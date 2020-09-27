c828b259b478b78f5465ef04b46dd3809a6dede4
Vihanflutter <62348193+Vihanflutter@users.noreply.github.com>
2020-09-27 11:20:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.list').click(function () {
       const value = $(this).attr('data-filter');
-      if (value == 'all') {
+      if (value == 'car') {
         $('.itemBox').show('1000');
       }
       else {
